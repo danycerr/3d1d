3ccb7098425dc85a789c9e45fc51cc30333677c4
danycerr <cerroni@mox31.mate.polimi.it>
2018-12-14 10:05:10 +0000

    assemmbly 17p17

--- src/13_nuclear/util/branch_generator.cpp
+++ src/13_nuclear/util/branch_generator.cpp
@@ -45,8 +45,8 @@ out.open("./seg.pts");
 out<<"BEGIN_LIST" <<std::endl;
 for(int b=0;b<branches.size();b++){
 out<<"BEGIN_ARC" <<std::endl;
-out<<"BC DIR  " << branches[b].val1 <<std::endl;
-out<<"BC DIR  " << branches[b].val2 <<std::endl;
+out<<"BC MIX  " << branches[b].val1 <<std::endl;
+out<<"BC MIX  " << branches[b].val2 <<std::endl;
 out<<"\t" << 1<<"\t"<<branches[b].x[0]<< "\t" << branches[b].y[0]<< "\t" << branches[b].z[0]<<"\t"<<"begin"<<std::endl;
 out<<"\t"  << 1<<"\t"
           << branches[b].x[branches[b].x.size()-1] << "\t"  
@@ -68,24 +68,40 @@ out.close();
 int main (){
     std::cout<<"Generator of branches"<<std::endl;
     
-    std::vector<double> p1={0.5,0.33,0}; double pt1d=0.77;
-    std::vector<double> p2={0.5,0.33,0.1}; double pt2d=1.;
-    branch b1(p1,p2,10, pt1d,pt2d);
+//     std::vector<double> p1={0.05,0.05,0.}; double pt1d=0.;
+//     std::vector<double> p2={0.05,0.05,1.}; double pt2d=0.;
+//     branch b1(p1,p2,10, pt1d,pt2d);
     std::vector<branch> branches;
-    branches.push_back(b1);
-    int ntheta=20;
-    double dtheta=360/ntheta;
-    double r=0.3, xc=p1[0], yc=p1[1], r2=0.2;    
-    for(int k=0;k<ntheta;k++){	
-     // std::vector<double> p3={r2*cos(k*dtheta)+xc,r2*sin(k*dtheta)+yc,0};double pt3d=0.77;
-     // std::vector<double> p4={r*cos(k*dtheta)+xc,r*sin(k*dtheta)+yc,0.1};double pt4d=0.;
-
-     std::vector<double> p3={(double)(rand() % 100)/100.,0.66*((double)(rand() % 100)/100.) ,0};double pt3d=0.77;
-     std::vector<double> p4={(double)(rand() % 100)/100.,0.66*((double)(rand() % 100)/100.) ,0.1};double pt4d=0.;
-
-    branch b2(p3,p4,10, pt3d,pt4d);
-    branches.push_back(b2);
-}
+//     branches.push_back(b1);
+//     radial wels
+//     int ntheta=20;
+//     double dtheta=360/ntheta;
+//     double r=0.3, xc=p1[0], yc=p1[1], r2=0.2;    
+//     for(int k=0;k<ntheta;k++){	
+//      // std::vector<double> p3={r2*cos(k*dtheta)+xc,r2*sin(k*dtheta)+yc,0};double pt3d=0.77;
+//      // std::vector<double> p4={r*cos(k*dtheta)+xc,r*sin(k*dtheta)+yc,0.1};double pt4d=0.;
+// 
+//      std::vector<double> p3={(double)(rand() % 100)/100.,0.66*((double)(rand() % 100)/100.) ,0};double pt3d=0.77;
+//      std::vector<double> p4={(double)(rand() % 100)/100.,0.66*((double)(rand() % 100)/100.) ,0.1};double pt4d=0.;
+// 
+//     branch b2(p3,p4,10, pt3d,pt4d);
+//     branches.push_back(b2);
+// }
+// //      assembly grid
+    int nrow=17; int ncol=17;
+    double dx=0.1/(nrow+1);
+    double dy=0.1/(ncol+1);
+    double x=0.; 
+    for(int ix=0;ix<nrow;ix++){
+       x+=dx;double y=0.;
+       for(int iy=0;iy<ncol;iy++){
+          y+=dy;
+	  std::vector<double> p_low={x,y,0}; double p_low_v=0.;
+	  std::vector<double> p_up={x,y,1};  double p_up_v=0.;
+	  branch rod(p_low,p_up,10, p_low_v,p_up_v);
+          branches.push_back(rod);
+      }
+    }
 
 print(branches); 
     
